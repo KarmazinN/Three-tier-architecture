7e01e82f272c021a37b2be7037a2a60fedb73490
Nazar <karmazinnazar@gmail.com>
2022-11-19 18:49:55 +0200

    Добавьте файлы проекта.

---
+++ DataLayer/EFDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.Entityes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    internal class EFDBContext : DbContext
+    {
+        public DbSet<Directory> Directories { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Page> Page { get; set; }
+
+        public EFDBContext(DbContextOptions<EFDBContext> options) : base(options) { }
+    }
+}
---
+++ DataLayer/Entityes/Directory.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entityes
+{
+    internal class Directory : Page
+    {
+        public List<Material> Materials { get; set; }
+    }
+}
